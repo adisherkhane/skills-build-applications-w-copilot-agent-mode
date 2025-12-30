e5e0d1a315ecb407a24b10349a433bf2b3c6c623
ADITYA V SHERKHANE <97685038+adisherkhane@users.noreply.github.com>
2025-12-30 20:01:04 +0000

    Start the Python Django app and check the output

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -26,8 +26,18 @@ SECRET_KEY = 'django-insecure-d&d@r8xrsb&ui-+c4)1c&g^phz6$mb_^tk#m9p*u=jfab+gib)
 DEBUG = True
 
 
-# Allow all hosts
-ALLOWED_HOSTS = ['*']
+
+# Allow codespace URL and localhost
+import os
+codespace_name = os.environ.get('CODESPACE_NAME')
+if codespace_name:
+    ALLOWED_HOSTS = [
+        f"{codespace_name}-8000.app.github.dev",
+        'localhost',
+        '127.0.0.1',
+    ]
+else:
+    ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
